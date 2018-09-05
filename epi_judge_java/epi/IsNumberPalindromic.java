00b9db02c71d162205d4f1f198627778d3183210
forsythe <fjorsyth@gmail.com>
2018-09-05 17:13:34 +0800

    IsNumberPalindromic.java 5.9

--- epi_judge_java/epi/IsNumberPalindromic.java
+++ epi_judge_java/epi/IsNumberPalindromic.java
@@ -1,18 +1,41 @@
 package epi;
+
 import epi.test_framework.EpiTest;
 import epi.test_framework.GenericTest;
-public class IsNumberPalindromic {
-  @EpiTest(testDataFile = "is_number_palindromic.tsv")
-  public static boolean isPalindromeNumber(int x) {
-    // TODO - you fill in here.
-    return true;
-  }
 
-  public static void main(String[] args) {
-    System.exit(
-        GenericTest
-            .runFromAnnotations(args, "IsNumberPalindromic.java",
-                                new Object() {}.getClass().getEnclosingClass())
-            .ordinal());
-  }
+public class IsNumberPalindromic {
+    @EpiTest(testDataFile = "is_number_palindromic.tsv")
+    public static boolean isPalindromeNumber(int x) {
+        // TODO - you fill in here.
+        if (x < 0)
+            return false;
+
+        int base = (int) Math.pow(10, Math.floor(Math.log10(x)));
+        while (x > 0) {
+            int lsd = x % 10;
+            int msd = x / base;
+
+//            System.out.printf("x=%d\n", x);
+//            System.out.printf("lsd=%d, msd=%d\n", lsd, msd);
+
+            if (lsd != msd)
+                return false;
+
+            x %= base;
+            x /= 10;
+//            System.out.printf("post x=%d\n", x);
+
+            base /= 100;
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        System.exit(
+                GenericTest
+                        .runFromAnnotations(args, "IsNumberPalindromic.java",
+                                new Object() {
+                                }.getClass().getEnclosingClass())
+                        .ordinal());
+    }
 }
--- readme.md
+++ readme.md
@@ -4,6 +4,8 @@
 * 5.1 Parity.java
 * 5.7 PowerXY.java
 * 5.8 ReverseDigits.java
+* 5.11 RectangleIntersection.java
+* 5.9 IsNumberPalindromic.java
+
 ## Pseudocode
 * 5.3 ReverseBits.java
-* 5.11 RectangleIntersection.java
